7e2d7cd4ef01140cf1e40e0828dda4c8ecf68717
Freddy Tomada <freddytomada7@gmail.com>
2026-05-20 21:31:28 -0400

    add tanstack query and home screen

---
+++ app/home/index.tsx
@@ -0,0 +1,16 @@
+import { useMovies } from "@/presentation/hooks/useMovies";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  const { nowPlayingQuery } = useMovies();
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+
+      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
